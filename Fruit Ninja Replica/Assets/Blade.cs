c01bfe41a5e4839ba7a69708b07619815aabc16d
Ariel <ar.i117@hotmail.com>
2018-02-24 23:40:56 -0600

    Add trail and blade

---
+++ Fruit Ninja Replica/Assets/Blade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+    public GameObject bladeTrailPrefab;
+
+    public bool isCutting = false;
+
+    GameObject currentBladeTrail;
+
+    Rigidbody2D rb;
+    Camera cam;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+
+        if (isCutting)
+        {
+            UpdateCut();
+        }
+	}
+
+    void UpdateCut()
+    {
+        rb.position = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void StartCutting ()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+    }
+
+    void StopCutting ()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2f);
+    }
+}
